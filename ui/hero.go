446b92e2b881b2984fc97fc6de9a39b0c75f5c6f
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2020-06-26 10:28:18 -0400

    ok enough item box

--- ui/hero.go
+++ ui/hero.go
@@ -13,16 +13,24 @@ import (
 )
 
 func DrawHeroItemsUI(mi characters.MindInput) {
-	canvas := pixelgl.NewCanvas(pixel.R(0, 0, 440, 42))
+	numItems := float64(5)
+	padding := float64(5)
+	w := float64((sheet.TileSize * numItems) + (padding * (numItems + 1)))
+	h := float64(sheet.TileSize + (padding * 2))
+
+	canvas := pixelgl.NewCanvas(pixel.R(0, 0, w, h))
 	canvas.Clear(color.Black)
 
 	hero := mi.GetCharacter("hero")
 
 	imd := imdraw.New(nil)
-	imd.Color = pixel.RGB(1, 0, 0)
 
-	for i := hero.InHands - 2; i <= hero.InHands+2; i++ {
-		pos := pixel.V(float64(((i+2)*64)+10), 10)
+	start := hero.InHands - int(numItems/2)
+	for loc := float64(0); loc < numItems; loc++ {
+		i := int(loc) + start
+		spot := (loc * sheet.TileSize) + (padding * loc)
+
+		pos := pixel.V(spot+padding, padding)
 		if i >= 0 && i < len(hero.Items) {
 			ci := hero.Items[i]
 			item := items.NewItem(ci.Name, pos, "")
@@ -30,14 +38,19 @@ func DrawHeroItemsUI(mi characters.MindInput) {
 			sprite.Draw(canvas, im.Scaled(pixel.ZV, 2).Moved(pos.Add(pixel.V(24, 24))))
 		}
 
+		if i == hero.InHands {
+			imd.Color = pixel.RGB(1, 0, 0)
+		} else {
+
+			imd.Color = pixel.RGB(1, 1, 1)
+		}
 		imd.Push(pos, pos.Add(pixel.V(sheet.TileSize, sheet.TileSize)))
 		imd.Rectangle(2)
-
 	}
 
 	imd.Draw(canvas)
 
 	bds := mi.GetWindowBounds()
 	x := bds.W() / 2
-	mi.AddCanvasStatic(canvas, pixel.V(x, 21))
+	mi.AddCanvasStatic(canvas, pixel.V(x, h/2))
 }
